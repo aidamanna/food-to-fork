86ae2c54553169fc8ea5620a3f2d5ad0ddc50dfb
Aida Manna <aida@typeform.com>
2016-04-15 09:01:43 +0200

    Delete data passed to env varialbles

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,5 +16,3 @@ Dotenv.load
 
 include RSpec::Matchers
 World(PageObject::PageFactory)
-
-$data = YAML.load_file(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'data', 'data.yml')))
